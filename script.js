d8e68f0ee9b93a86cff9797e2cbc9f4bda05302d
aya0620a <aotomiyabi2525@gmail.com>
2024-03-08 15:49:28 +0900

    表示されるように変更

--- script.js
+++ script.js
@@ -2,3 +2,38 @@
 function start(){
     alert("Hello World!");
 }
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+const gameboard = document.getElementById("gameboard");
+
+window.onload = function(){
+    let shuffledCards = []; // This is the array that will hold the shuffled cards
+    for(let i = 0; i < 32; i++){
+        shuffledCards.push(cards[i]);
+        shuffledCards.push(cards[i]);
+    }
+    shuffle(shuffledCards);
+
+    for(let i = 0; i < 32; i++){
+        const card = document.createElement("div");
+        card.className = "cardback";
+        card.id = "card" + i;
+        card.number = shuffledCards[i];
+        card.src = "cardback.png";
+        document.getElementById("gameboard").appendChild(card);
+        }
+}
+
+
+function shuffle(shuffledCards){
+    let j = shuffledCards.length;
+    let temp;
+
+    for(let i = 0; i < 32; i++){
+        j = Math.floor(Math.random() * (i + 1));
+        temp = shuffledCards[j];
+        shuffledCards[j] = shuffledCards[i];
+        shuffledCards[i] = temp;
+    }
+}
