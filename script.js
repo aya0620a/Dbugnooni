7b411383ec085f0f181b895e04556926294e9e3a
YugoMasuda <yugo.m.0601@gmail.com>
2024-03-08 13:39:52 +0900

    ty

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function start(){
     alert("Hello World!");
 }
+//スタート画面でボタンを押したときの処理
 const button = document.getElementById('button');
 button.addEventListener('click',function(){
     location.href = "index2.html";
