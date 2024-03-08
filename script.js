5e1e90f3cd72e205c28c8420d04988dbb1993864
YugoMasuda <yugo.m.0601@gmail.com>
2024-03-08 11:47:41 +0900

    'third'

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-
 function start(){
     alert("Hello World!");
 }
+const button = document.getElementById('button');
+button.addEventListener('click',function(){
+    location.href = "input.html";
+})
