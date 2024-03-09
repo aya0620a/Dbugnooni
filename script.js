8280d37f4f9a2b1ccc4182cc4cacf3f3e28d6a1b
kanon2004 <nonnonnyanko@icloud.com>
2024-03-09 15:27:02 +0900

    t

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ nextbutoon.addEventListener('click',function(){
 
 
 
+window.onload = function(){
+    const heading = document.querySelector('win');
+
+    const keyframes={
+        opacity:[0,1],
+        translate:['0,50px',0],
+    };
+    const options={
+        duration:2000,
+        easing:'ease',
+    };
+    heading.animate(keyframes,options);
+}
+
+
