0fd95971980e75adbede0658b0b81142ba03ce97
aya0620a <aotomiyabi2525@gmail.com>
2024-03-09 12:08:05 +0900

    表の表示を追加、取れる組を追加

--- script.js
+++ script.js
@@ -1,5 +1,85 @@
 //カードの作成
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+const cards = [{
+    "name": "ENTP",
+    "num": 0,
+    "img": "ENTPcard.png",
+    "class": "cardback"
+},{
+    "name": "INTP",
+    "num": 1,
+    "img": "INTPcard.png",
+    "class": "cardback"
+},{
+    "name": "ENTJ",
+    "num": 2,
+    "img": "ENTJcard.png",
+    "class": "cardback"
+},{
+    "name": "INTJ",
+    "num": 3,
+    "img": "INTJcard.png",
+    "class": "cardback"
+},{
+    "name": "ENFP",
+    "num": 4,
+    "img": "ENFPcard.png",
+    "class": "cardback"
+},{
+    "name": "INFP",
+    "num": 5,
+    "img": "INFPcard.png",
+    "class": "cardback"
+},{
+    "name": "ENFJ",
+    "num": 6,
+    "img": "ENFJcard.png",
+    "class": "cardback"
+},{
+    "name": "INFJ",
+    "num": 7,
+    "img": "INFJcard.png",
+    "class": "cardback"
+},{
+    "name": "ISFP",
+    "num": 8,
+    "img": "ISFPcard.png",
+    "class": "cardback"
+},{
+    "name": "ESFP",
+    "num": 9,
+    "img": "ESFPcard.png",
+    "class": "cardback"
+},{
+    "name": "ISFJ",
+    "num": 10,
+    "img": "ISFJcard.png",
+    "class": "cardback"
+},{
+    "name": "ESFJ",
+    "num": 11,
+    "img": "ESFJcard.png",
+    "class": "cardback"
+},{
+    "name": "ISTP",
+    "num": 12,
+    "img": "ISTPcard.png",
+    "class": "cardback"
+},{
+    "name": "ESTP",
+    "num": 13,
+    "img": "ESTPcard.png",
+    "class": "cardback"
+},{
+    "name": "ISTJ",
+    "num": 14,
+    "img": "ISTJcard.png",
+    "class": "cardback"
+},{
+    "name": "ESTJ",
+    "num": 15,
+    "img": "ESTJcard.png",
+    "class": "cardback"
+}];
 
 let currentUsers = 0;
 
@@ -14,20 +94,25 @@ let users = [{
     "score": 0
 },]
 
+const usersuu = users.length;
+
 const gameboard = document.getElementById("gameboard");
 
-//カードをシャッフルする
+//カードを複製しシャッフルする
 const createShuffledCards = () => {
-    let shuffledCards = []; // This is the array that will hold the shuffled cards
-    for(let i = 0; i < 32; i++){
-        shuffledCards.push(cards[i]);
-        shuffledCards.push(cards[i]);
+    let shuffledCards = []; 
+    for(let i = 0; i < 16; i++){
+        let temp1 = {...cards[i]};
+        let temp2 = {...cards[i]};
+        shuffledCards.push(temp1);
+        shuffledCards.push(temp2);
     }
     shuffle(shuffledCards);
 
     return shuffledCards;
 }
 
+//カードをクリックしたときの処理
 const onclick = (e) => {
     turn(e);
 }
@@ -35,38 +120,45 @@ const onclick = (e) => {
 //裏返しのカードを表示
 const displayCards = (shuffledCards) => {
     for(let i = 0; i < 32; i++){
-        const card = document.createElement("div");
-        card.onclick = onclick;
-        card.className = "cardback";
-        card.id = "card" + i;
-        card.number = shuffledCards[i];
-        document.getElementById("gameboard").appendChild(card);
+        const card = shuffledCards[i];
+        const div = document.createElement("div");
+        div.onclick = onclick;
+        div.className = card.class;
+        div.id = "card" + i;
+        div.number = card.num;
+        div.index = i;
+        document.getElementById("gameboard").appendChild(div);
     }
 }
 
+//カードをシャッフルする
 function shuffle(shuffledCards){
-    let j = shuffledCards.length;
-    let temp;
-
     for(let i = 0; i < 32; i++){
-        j = Math.floor(Math.random() * (i + 1));
-        temp = shuffledCards[j];
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = shuffledCards[j];
         shuffledCards[j] = shuffledCards[i];
         shuffledCards[i] = temp;
     }
 }
 
+let shuffledCards;
+
 //画面が表示されたときに実行される
 window.onload = function(){
-    const shuffledCards = createShuffledCards();
+    let username = document.getElementById("nextplayer");
+    username.innerHTML = `${users[currentUsers].name}さんの番です`;
+    shuffledCards = createShuffledCards();
 
     displayCards(shuffledCards);
 }
 
-
+//カードを一枚引いたかどうかの判定
 let flgFirst = true; 
+
+//連続処理防止
 let backTimer;
 
+//裏返しの処理
 function turn(e){
     let div = e.target;
 
@@ -74,8 +166,9 @@ function turn(e){
         return;
     };
     if(div.innerHTML === ""){
-        div.className = "card";
-        // div.innerHTML = '<img src="ESFP.png">';
+        div.className = "cardface";
+        shuffledCards[div.index].class = "cardface";
+        div.innerHTML = `<img src="img/${cards[div.number].img}">`;
     }else{
         return;
     }
@@ -83,12 +176,31 @@ function turn(e){
         firstcard = div;
         flgFirst = false;
     }else{
-        if(firstcard.number % 2 == 0){
+        if(firstcard.number === div.number){
             users[currentUsers].score++;
             console.log(users[currentUsers].score);
             backTimer = setTimeout(function(){
                 div.className = "cardfinish";
+                shuffledCards[div.index].class = "cardfinish";
                 firstcard.className = "cardfinish";
+                shuffledCards[firstcard.index].class = "cardfinish";
+                div.innerHTML = "";
+                firstcard.innerHTML = "";
+                backTimer = NaN;
+                // if(unitCounts === 16){
+                //     alert("Game Clear");
+                // }
+            }, 500);
+        }else if((firstcard.number%8) === (div.number%8)){
+            users[currentUsers].score+=2;
+            console.log(users[currentUsers].score);
+            backTimer = setTimeout(function(){
+                div.className = "cardfinish";
+                shuffledCards[div.index].class = "cardfinish";
+                firstcard.className = "cardfinish";
+                shuffledCards[firstcard.index].class = "cardfinish";
+                div.innerHTML = "";
+                firstcard.innerHTML = "";
                 backTimer = NaN;
                 // if(unitCounts === 16){
                 //     alert("Game Clear");
@@ -97,19 +209,21 @@ function turn(e){
         }else{
             backTimer = setTimeout(function(){
                 div.className = "cardback";
+                shuffledCards[div.index].class = "cardback";
                 div.innerHTML = "";
                 firstcard.className = "cardback";
+                shuffledCards[firstcard.index].class = "cardback";
                 firstcard.innerHTML = "";
                 firstcard = null;
                 backTimer = NaN;
-                currentUsers = (currentUsers + 1) % 3;
+                currentUsers = (currentUsers + 1) % usersuu;
+                let username = document.getElementById("nextplayer");
+                username.innerHTML = `${users[currentUsers].name}さんの番です`;
             }, 500);
         };
         flgFirst = true;
         console.log(currentUsers);
         console.log(users);
     }
-
-
 }
 
